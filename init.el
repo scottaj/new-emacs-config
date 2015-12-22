534f864d0e8b264e63bf83e618892d80dda18e14
Al Scott <al.scott@atomicobject.com>
2015-12-22 00:25:54 -0700

    Dash support.

--- init.el
+++ init.el
@@ -1,5 +1,8 @@
 (load-file "~/.emacs.d/package-setup.el")
+
 (load-directory "~/.emacs.d/package-config")
+
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
+
 (load-file "~/.emacs.d/user.el")
---
+++ package-config/dash-at-point-config.el
@@ -0,0 +1,3 @@
+(add-to-list 'load-path "~/.emacs.d/dash-at-point")
+(autoload 'dash-at-point "dash-at-point"
+          "Search the word at point with Dash." t nil)
