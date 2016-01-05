2e4c80143b0ae60af64337fa9bed98f275bface8
Al Scott <al.scott@atomicobject.com>
2016-01-05 10:33:30 -0500

    Make buffer name readable when there are flycheck issues

--- custom.el
+++ custom.el
@@ -45,4 +45,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(mode-line ((t (:background "#073642" :box (:line-width 1 :color "#839496") :weight normal :height 0.5)))))
+ '(mode-line ((t (:background "#073642" :box (:line-width 1 :color "#839496") :weight normal :height 0.5))))
+ '(mode-line-buffer-id ((t (:background "#073642" :foreground "#d33682")))))
